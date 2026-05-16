a5876c5d99d42fecf0fbfc8d1b87d75c578d9f45
sif0918183-design <sif0918183@gmail.com>
2026-05-16 12:56:33 +0200

    Update main.dart

--- pwa_to_flutter_app/lib/main.dart
+++ pwa_to_flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart' as fln;
 import 'package:permission_handler/permission_handler.dart';
